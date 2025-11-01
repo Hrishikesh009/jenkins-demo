1fb0f7dc347de27aa43a3fbe21800f48586b72ab
Hrishikesh <78008167+Hrishikesh009@users.noreply.github.com>
2025-11-01 17:24:02 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,6 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello from Jenkins + GitHub! v2");
+        System.out.println("Hello from Jenkins + GitHub! v1");
     }
 
 }
